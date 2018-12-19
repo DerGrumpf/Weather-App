704e907203d73ee9d6bfeb9560420d9ec8cede9c
DerGrumpf <phil.keier@hotmail.com>
2018-12-19 13:10:05 +0100

    Add Class Definition

--- communication.py
+++ communication.py
@@ -1,3 +1,7 @@
 ''' Communicate with esp8266 Object
 De- & Encodes request data
 Copyright (c) 2018 by DerGrumpf. All Rights Reserved. '''
+
+class Communicator:
+    def __init__(self):
+        pass
