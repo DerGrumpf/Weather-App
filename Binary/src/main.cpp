fe883c5c0e3ad01ba5b1793edc6262b84816d960
DerGrumpf <phil.keier@hotmail.com>
2018-12-21 18:13:18 +0100

    Add ESP Source

--- Binary/src/main.cpp
+++ Binary/src/main.cpp
@@ -1,9 +1,50 @@
-#include <Arduino.h>
+#include <ESP8266WiFi.h>
+
+char* host = "host";
+char* pass = "pass";
+
+void connect_success() {
+  Serial.print("Your IP is: ");
+  Serial.println(WiFi.localIP());
+}
+
+void connect_error() {
+  Serial.print("Start Reconnect");
+  WiFi.begin(host, pass);
+  while (WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println();
+
+  if (WiFi.status() == WL_CONNECTED) connect_success();
+  else connect_error();
+}
+
+void connect() {
+  WiFi.begin(host, pass);
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println();
+
+  if (WiFi.status() == WL_CONNECTED) connect_success();
+  else connect_error();
+}
+
+//void handle_success() {}
+
+//void handle_error() {}
+
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println();
+  connect();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop() {}
